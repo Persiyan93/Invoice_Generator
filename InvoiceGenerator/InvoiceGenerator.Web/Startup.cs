7f5a6537bcd021cbe48a540f60168e7cec7815b9
Persiyan93 <persiyan.georgiev@gmail.com>
2021-06-03 08:27:19 +0300

    Configure Identity and JWT

--- InvoiceGenerator/InvoiceGenerator.Web/Startup.cs
+++ InvoiceGenerator/InvoiceGenerator.Web/Startup.cs
@@ -1,11 +1,18 @@
+using InvoiceGenerator.Data;
+using InvoiceGenerator.Data.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace InvoiceGenerator.Web
 {
@@ -21,6 +28,42 @@ namespace InvoiceGenerator.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Connect app with MS SQL SERVER
+            services.AddDbContext<ApplicationDbContext>(options =>
+
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+
+
+
+            //Add Identity with JWT 
+            services.AddIdentity<ApplicationUser, ApplicationRole>(IdentityOptionsProvider.GetIdentityOptions)
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["JWT:ValidAudience"],
+                    ValidIssuer = Configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                };
+
+            });
+
+
+
+
             services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory
