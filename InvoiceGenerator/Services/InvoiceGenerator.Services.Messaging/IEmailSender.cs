fe8f06aec07ef7eb4d244b3ccb0d776039e18a40
Persiyan93 <persiyan.georgiev@gmail.com>
2021-06-08 16:40:57 +0300

    Add AutoMapper and Messaging service

---
+++ InvoiceGenerator/Services/InvoiceGenerator.Services.Messaging/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace InvoiceGenerator.Services.Messaging
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string to, string subject, string htmlContent);
+    }
+}
