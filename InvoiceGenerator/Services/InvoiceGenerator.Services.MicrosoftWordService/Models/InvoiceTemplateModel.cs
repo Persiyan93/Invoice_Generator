89b749292f7fe751faabcdab8656b7682dd1b7c1
Persiyan93 <persiyan.georgiev@gmail.com>
2021-06-28 19:37:31 +0300

    View Models

---
+++ InvoiceGenerator/Services/InvoiceGenerator.Services.MicrosoftWordService/Models/InvoiceTemplateModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceGenerator.Services.MicrosoftWordService.Models
+{
+    public class InvoiceTemplateModel
+    {
+        public string Number { get; set; }
+
+        public string    BuyerCompanyName { get; set; }
+
+        public string BuyerCompanyType { get; set; }
+
+        public string BuyerVatNumber { get; set; }
+
+        public string BuyerCountryName { get; set; }
+
+        public string BuyerTownName { get; set; }
+
+        public string BuyerAddressText { get; set; }
+
+        public string BuyerUniqueIdentificationNumbe { get; set; }
+
+        public string BuyerAccontablePersonName { get; set; }
+
+        public string SellerVatNumber { get; set; }
+
+        public string SellerCompanyName { get; set; }
+
+        public string SellerCompanyType { get; set; }
+
+        public string SellerCountryName { get; set; }
+
+        public string SellerTownName { get; set; }
+
+        public string SellerAddressText { get; set; }
+
+        public string SellerUniqueIdentificationNumbe { get; set; }
+
+        public string SellerAccontablePersonName { get; set; }
+
+        public string Price { get; set; }
+
+        public string  VatValue { get; set; }
+
+        public string DateOfIssue { get; set; }
+
+        public string MethodOfPayment { get; set; }
+
+        public string DateOfTaxEvent { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string ArticleName { get; set; }
+
+        public string ArticlePrice { get; set; }
+
+        public string ArticleQuantity { get; set; }
+
+        public string ArticleVatValue { get; set; }
+
+
+
+
+
+
+    }
+}
--- InvoiceGenerator/Web/InvoiceGenerator.Web.Models/Invoices/InvoiceInputModel.cs
+++ InvoiceGenerator/Web/InvoiceGenerator.Web.Models/Invoices/InvoiceInputModel.cs
@@ -10,14 +10,28 @@ namespace InvoiceGenerator.Web.Models.Invoices
 {
     public class InvoiceInputModel
     {
-        public string CompanyId { get; set; }
+        public string SellerId { get; set; }
 
         public string ClientId { get; set; }
 
+        public  string ContactPersonId { get; set; }
+
+        public DateTime IssueDate { get; set; }
+
+        public TimeSpan?  PaymentTerm { get; set; }
+
         public ICollection<ArticleToInvoiceInputModel > Articles { get; set; }
 
-        public DateTime MaturityDate { get; set; }
+        public DateTime DateOfTaxEven { get; set; }
+
+        public MethodsOfPayment PaymentMethod { get; set; }
 
         public LanguageOfInvoice Language { get; set; }
+
+        public double DiscountPercentage { get; set; }
+
+        public AdditionalInvoiceOptions AdditionalOptions { get; set; }
+
+
     }
 }
