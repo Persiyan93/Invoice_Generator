b5f22d8bf5da927bf5ee377d4e9fc375fa5ef38a
Persiyan93 <persiyan.georgiev@gmail.com>
2021-07-03 07:49:09 +0300

    Global constants and Messages

--- InvoiceGenerator/Common/InvoiceGenerator.Common/SuccessMessages.cs
+++ InvoiceGenerator/Common/InvoiceGenerator.Common/SuccessMessages.cs
@@ -8,12 +8,16 @@ namespace InvoiceGenerator.Common
 {
    public static class SuccessMessages
     {
+        
+        public const string SuccessfullyStatus = "Successful";
         public const string SuccessfullyAddedUser = "{0} is successfully registered!";
         public const string SuccessfullyCreatedCompany = "Company with Id:{0} was successfully create!";
         public const string SuccessfullyCreatedClient = "Client with Id:{0} was successfully create!";
         public const string SuccessfullyAddedAddress = "Address with Id:{0} was syccessfully added";
         public const string SuccessfullyAddedMailingAddress = "Mailing address was added successfully!";
         public const string SuccessfullyAddedContactPerson= "Person with Id:{0} was added successfully to contact list!";
+        public const string SuccessfullyAddedArticle= "Article with Id:{0} was added successfully!";
+        public const string SuccessfullyCreatedInvoice= "Invoice with Id:{0} was created successfully";
         
 
     }
--- InvoiceGenerator/InvoiceGenerator.Web/Controllers/InvoiceController.cs
+++ InvoiceGenerator/InvoiceGenerator.Web/Controllers/InvoiceController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using InvoiceGenerator.Common;
+using InvoiceGenerator.Data.Models;
+using InvoiceGenerator.Services.Data;
+using InvoiceGenerator.Web.Models;
+using InvoiceGenerator.Web.Models.Invoices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,7 +16,56 @@ namespace InvoiceGenerator.Web.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class InvoiceController : ControllerBase
+
+
     {
+        private readonly IInvoiceService invoiceService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public InvoiceController(IInvoiceService invoiceService,UserManager<ApplicationUser> userManager)
+        {
+            this.invoiceService = invoiceService;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateInvoice(InvoiceInputModel inputModel)
+        {
+            
+            var userId =  userManager.GetUserId(User);
+            var invoiceId=await invoiceService.CreateInvoice(inputModel,userId);
+
+            return this.Ok(new ResponseViewModel
+            {
+                Status = SuccessMessages.SuccessfullyStatus,
+                Message = string.Format(SuccessMessages.SuccessfullyCreatedInvoice, invoiceId)
+            });
+
+            
+            
+        }
+
+
+        [HttpGet("{invoiceId}")]
+        public async Task<IActionResult> GetInvoiceById(string invoiceId) 
+        {
+            var invoice = await invoiceService.GetInvoiceById<InvoiceViewModel>(invoiceId);
+
+            return this.Ok(invoice);
+
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAll()
+        {
+            var userid = userManager.GetUserId(this.User);
+            var invoices = await invoiceService.GetAllCompanyInvoices<InvoiceInLIstVIewModel>(userid);
+
+            return this.Ok(invoices);
+        }
+ 
     }
 }
