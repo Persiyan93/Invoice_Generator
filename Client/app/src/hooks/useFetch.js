975d3e751d55b63fd4bb9bda2c9151dc85c90e22
Persiyan93 <persiyan.georgiev@gmail.com>
2021-07-22 09:00:33 +0300

    Add trigger for fetching data

--- Client/app/src/hooks/useFetch.js
+++ Client/app/src/hooks/useFetch.js
@@ -4,52 +4,60 @@ import * as dataService from '../services/dataService'
 import * as globalServices from '../services/globalServices'
 
 // 
-const useFetch = (endpoint, data, method) => {
+const useFetch = (endpoint, triger,method='GET', data ) => {
     const [response, setResponse] = useState();
 
     let history = useHistory();
 
     useEffect(() => {
-        if (method === 'post') {
-            dataService.post(data, endpoint)
-                .then(res => res.json())
-                .then(res => {
-                    if (res.status == "Unsuccessful") {
-                        console.log('Unsuccessful status ')
-                        console.log(res);
-                    }
-                    else {
-                        let id = globalServices.getIdFromResponse(res.message);
-                        setResponse(id)
-                    }
+        if (triger != '') {
+            console.log(triger)
+            console.log(method)
+            if (method == 'POST') {
+                console.log('Inside post')
+                dataService.post(data, endpoint)
+                    .then(res => res.json())
+                    .then(res => {
+                        if (res.status == "Unsuccessful") {
+                            console.log('Unsuccessful status ')
+                            console.log(res);
+                        }
+                        else {
+                            let id = globalServices.getIdFromResponse(res.message);
+                            setResponse(id)
+                        }
 
-                })
-                .catch(err => {
-                    console.log(err)
-                    history.push('/Errors/ConnectionError')
-                })
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        history.push('/Errors/ConnectionError')
+                    })
 
+            }
+            else {
+                console.log(endpoint)
+                dataService.get(endpoint)
+                    .then(res => res.json())
+                    .then(res => {
+                        if (res.status == "Unsuccessful") {
+                            console.log('Unsuccessful status ')
+                            console.log(res);
+                        }
+                        else {
+                            console.log(res)
+                            setResponse(res)
+                        }
+
+                    })
+                    .catch(err => {
+                        
+                        console.log(err)
+                        history.push('/Errors/ConnectionError')
+                    })
+            }
         }
-        else {
-            dataService.get(endpoint)
-                .then(res => res.json())
-                .then(res => {
-                   if (res.status == "Unsuccessful") {
-                        console.log('Unsuccessful status ')
-                        console.log(res);
-                    }
-                    else {
-                        setResponse(res)
-                    }
 
-                })
-                .catch(err => {
-                    console.log(err)
-                    history.push('/Errors/ConnectionError')
-                })
-        }
-
-    }, []);
+    }, [triger]);
 
 
 
