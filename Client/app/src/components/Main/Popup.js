0401e6a6108a5e39bffde1dd1c71f49e5ec204fe
Persiyan93 <persiyan.georgiev@gmail.com>
2021-07-22 08:59:46 +0300

    Add close button

--- Client/app/src/components/Main/Popup.js
+++ Client/app/src/components/Main/Popup.js
@@ -1,19 +1,52 @@
 import React from 'react'
-import { Dialog, DialogTitle, DialogContent, Typography } from '@material-ui/core';
+import { Dialog, DialogTitle, DialogContent, Typography, IconButton, withStyles } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import PeopleIcon from '@material-ui/icons/People';
 
+const useStyles = (thema => ({
+    root: {
+        '& .MuiFormControl-root': {
+            width: '90%',
+            margin: thema.spacing(1)
+
+        },
+      
+
+    },
+    button: {
+        marginLeft: '74%',
+        
+    }
+   
+}))
 class Popup extends React.Component {
     constructor(props) {
         super(props)
+        this.clickHandler=this.clickHandler.bind(this);
+    }
+    clickHandler(){
+        this.props.setOpenPopup(false);
     }
 
+
     render() {
-        const { title, children, openPopup, setOpenPopup ,width } = this.props;
+        const { title, children, openPopup, setOpenPopup, width, classes } = this.props;
         return (
-            <Dialog open={openPopup} maxWidth={width} fullWidth={true}  >
-                <DialogTitle> 
+            <Dialog open={openPopup} maxWidth={width} fullWidth={true}   >
+                <DialogTitle>
+               
                     <Typography variant="h6">
+                    <   IconButton  color="primary" size='large' >
+                            <PeopleIcon color="primary"  size="large"/>
+                        </IconButton>
                         {title}
+                       
+                        <IconButton  color="secondary" className={classes.button} onClick={this.clickHandler}>
+                            <CloseIcon color="secondary" />
+                        </IconButton>
                     </Typography>
+                   
+
                 </DialogTitle>
                 <DialogContent dividers>
                     {children}
@@ -24,4 +57,4 @@ class Popup extends React.Component {
 }
 
 
-export default Popup
+export default withStyles(useStyles)(Popup)
