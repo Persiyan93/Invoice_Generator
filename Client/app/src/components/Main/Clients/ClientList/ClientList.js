c1858edc19b1c59163e0699d7a37931a6636d219
Persiyan93 <persiyan.georgiev@gmail.com>
2021-08-15 21:10:49 +0300

    Add client list Component

--- Client/app/src/components/Main/Clients/ClientList/ClientList.js
+++ Client/app/src/components/Main/Clients/ClientList/ClientList.js
@@ -1,297 +1,153 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles, Button, Table, InputBase, Paper, Typography, TableRow, TableBody, Toolbar, TableSortLabel, TextField, InputAdornment, TablePagination } from '@material-ui/core/';
-
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import SearchIcon from '@material-ui/icons/Search';
-import * as clientsService from '../../../../services/clientsService';
+import { useEffect, useState, } from 'react'
+import { Link } from 'react-router-dom'
+import {
+    IconButton,
+    makeStyles, Button, Table, Grid, InputBase, Paper, TableCell, TableRow, TableBody, Toolbar, Divider, TextField, InputAdornment, TablePagination
+} from '@material-ui/core/';
+import DeleteIcon from '@material-ui/icons/Delete';
+import InfoIcon from '@material-ui/icons/Info';
+import useFetchGet from '../../../../hooks/useFetchGet'
+import useFetchPut from '../../../../hooks/useFetchPut'
+import apiEndpoints from '../../../../services/apiEndpoints'
 import ClientRow from './ClientRow'
-import reactDom from 'react-dom';
-const useStyles = (theme => ({
-    root: {
-        '& > *': {
-            borderBottom: 'unset',
-        },
-    },
-    table: {
-        '& thead th': {
-            fontWeight: '600',
+import TableWithServerSidePagingAndSorting from '../../Elements/TableWithServerSidePagingAndSorting'
+import ConfirmationPopup from '../../Popup/ConfirmPopup'
+import SearchBar from '../../Elements/SearchBar';
+import queryString from "query-string";
+
+const headCells = [
+    { id: 1, title: '', disableSorting: 'true' },
+    { id: 'Name', title: 'Име на фирмата' },
+    { id: 'VatNumber', title: 'ДДС Номер' },
+    { id: 'Status', title: 'Статус' },
+    { id: 'CountOfInvoices', title: 'Брой на всички фактури' },
+    { id: 'CountOfOverdueInvoices', title: 'Брой на просрочени фактури' },
+    { id: 'ValueSumOfAllUnPaidInvoices', title: 'Стойност на неплатените фактури' },
+    { id: 5, title: 'Действие', disableSorting: 'true' },
+
+]
+
+const pages = [5, 10, 15, 20]
+export default function ClientList(props) {
+    const [openConfirmationPopup, setOpenConfirmationPopup] = useState(false);
+    const [paging, setPaging] = useState({ page: 0, rowsPerPage: 10 })
+    const [filterString, setFilterString] = useState('')
+    const [pagingAndSorting, setPagingAndSorting] = useState({ order: 'asc', orderBy: '' })
+    const [clients, setClients] = useState([])
+    const [selectedUserId, selectUserId] = useState('');
+    const [clientNewStatus, setClientNewStatus] = useState('')
 
 
-        },
-        '& tbody td': {
-            fontWeight: '300',
+    //Get clients
+    const [getClientsTringer, setGetClientsTriger] = useState(true)
+    var getClientsUrl = apiEndpoints.allClients + props.history.location.search
+    useFetchGet(getClientsUrl, setClients, getClientsTringer, setGetClientsTriger)
 
-        }, '& tbody tr:hover': {
-            backgroundColor: '#DAD4D3',
-            cursor: 'pointer'
+    // Update client status
+    const [updateClientStatusTriger, setUpdateClientStatusTriger] = useState(false);
+    useFetchPut(apiEndpoints.updateClientStatus, updateClientStatusTriger, setUpdateClientStatusTriger, { clientId:selectedUserId, status: clientNewStatus });
 
-        }
-    },
-    searchInput: {
-        opacity: '0.6',
-        padding: '0px,5px',
-        fontSize: '0.6rem',
-        width: '70%',
-        '&:hover': {
-            backgroundColor: '#f2f2f2'
-        },
-        '& .MuiSvgIcon-root': {
-            marginRight: '8px'
-        }
-    },
-    menu: {
-        marginBottom: '20px',
-        borderRadius: '20px'
-    }
-
-}))
-
-
-class ClientList extends React.Component {
-
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            page: 0,
-            rowsPerPage: 5,
-            order: '',
-            orderBy: '',
-            filterFunc: { fn: items => { return items } },
-            filterString: '',
-            clients: [{
-                id: "",
-                name: "",
-                vatNumber: "",
-                countOfInvoice: "",
-                countOfOverdueInvoices: "",
-                invoices: []
-            }]
-        };
-        this.clientsAfterPagingAndSorting = this.clientsAfterPagingAndSorting.bind(this);
-        this.handleChangePage = this.handleChangePage.bind(this)
-        this.rowsPerPageHandle = this.rowsPerPageHandle.bind(this);
-        this.handleSortRequest = this.handleSortRequest.bind(this);
-        this.handleSearch = this.handleSearch.bind(this);
-    }
-
-    componentDidMount() {
-
-        clientsService.getAllClients()
-            .then(res => res.json())
-            .then(res => {
-                if (res.status == "Unsuccessful") {
-                    console.log('Unsuccessful status ')
-                    console.log(res);
-                }
-                else {
-                    this.setState({ clients: res })
-                }
-
-            })
-            .catch(err => {
-                this.props.history.push('/Errors/ConnectionError')
-            })
-    }
-
-
-
-    handleChangePage(event, newPage) {
-        this.setState({ page: newPage });
-    }
-    rowsPerPageHandle(event) {
-        this.setState({ rowsPerPage: parseInt(event.target.value, 10) })
-        this.setState({ page: 0 });
-    }
-
-    clientsAfterPagingAndSorting(event) {
-        const { page, rowsPerPage, order, orderBy } = this.state;
-        console.log(this.state.filterFunc.fn(this.state.clients))
-        return this.stableSort(this.state.filterFunc.fn(this.state.clients), this.getComperator(order, orderBy))
-            .slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+    function handleChangePage(event, newPage) {
+        setPaging(prevState => ({ ...prevState, page: newPage }))
 
     }
-    getComperator(order, orderBy) {
-        return order == 'desc'
-            ? (a, b) => this.descendingComparator(a, b, orderBy)
-            : (a, b) => -this.descendingComparator(a, b, orderBy)
-    }
-    descendingComparator(a, b, orderBy) {
-        if (b[orderBy] < a[orderBy]) {
-            return -1;
-        }
-        if (b[orderBy] > a[orderBy]) {
-            return 1;
-        }
-        return 0;
-    }
-    stableSort(clients, comparator) {
-
-        const stabilizedThis = clients.map((el, index) => [el, index]);
-        stabilizedThis.sort((a, b) => {
-            const order = comparator(a[0], b[0]);
-            if (order != 0) return order;
-            return a[1] - b[1];
-        });
-        return stabilizedThis.map((el) => el[0]);
-    }
-
-    handleSortRequest(cellId) {
-        const { orderBy, order } = this.state
-        const isAsc = orderBy == cellId && order == "asc"
-        console.log(isAsc);
-        if (isAsc) {
-            this.setState({ order: 'desc' })
-        }
-        else {
-            this.setState({ order: 'asc' })
-        }
-        this.setState({ orderBy: cellId })
+    function trowsPerPageHandle(event) {
+        setPaging(prevState => ({ page: 0, rowsPerPage: parseInt(event.target.value, 10) }))
 
 
     }
 
-    handleSearch(event) {
-        let target = event.target;
-        const { clients } = this.state
-        this.setState({ filterString: target.value })
-        console.log(target.value)
+    function blockClientHandler(e, clientId) {
+        selectUserId(clientId);
+        setClientNewStatus('Blocked')
+        setOpenConfirmationPopup(true)
 
-        this.setState({
-            filterFunc: {
-                fn: clients => {
-                    if (target.value == '') {
-                        return clients;
-                    }
-                    else {
-                        console.log('Inside here')
-                        return clients.filter(x => x.name.toLowerCase().includes(target.value.toLowerCase()))
-                    }
-                }
-            }
+    }
+    function activateClientHandler(e, clientId) {
+        selectUserId(clientId);
+        setClientNewStatus('Active')
+        setUpdateClientStatusTriger(true)
+    }
 
+
+
+    const { orderBy, order } = pagingAndSorting;
+    const { page, rowsPerPage } = paging;
+    useEffect(() => {
+        props.history.replace({
+            search: `?${queryString.stringify({
+                page: page ? page : undefined,
+                rowsPerPage: rowsPerPage === 10 ? undefined : rowsPerPage,
+                order: order == 'asc' ? undefined : order,
+                orderBy: orderBy ? orderBy : undefined,
+                filterString: filterString ? filterString : undefined
+
+
+            })}`
         })
-        console.log(this.state)
+        setGetClientsTriger(true)
+    }, [order, orderBy, page, rowsPerPage, filterString])
+
+    function searchHandler(event) {
+        setFilterString(event.target.value)
     }
+    return (
+        <>
 
 
-    render() {
+            <SearchBar
+                title="Клиент"
+                searchbarLabel="Намери клиент"
+                placeholder="Име на клиента"
+                searchHandler={searchHandler}
+                filterString={filterString}
+            />
 
-        const pages = [5, 10, 20]
-        const { classes } = this.props
-        const { clients, page, rowsPerPage, orderBy, order, filterString } = this.state
-        const headCells = [
-            { id: 'name', title: 'Име на фирмата' },
-            { id: 'vatNumber', title: 'ДДС Номер' },
-            { id: 'countOfInvoice', title: 'Брой на всички фактури' },
-            { id: 'countOfOverdueInvoices', title: 'Брой на просрочени фактури' },
-            { id: 5, title: 'Действие', disableSorting: 'true' },
-        ]
-        return (
-            <  >
-                <Paper variant="outlined" className={classes.menu} elevation={20}>
-                    <h1 style={{ textAlign: 'center' }}>Клиенти</h1>
-                    <Toolbar>
-                        <TextField className={classes.searchInput}
-                            value={filterString}
-                            label="Намери клиент"
-                            variant="outlined"
-                            placeholder="Име на клиент "
-                            InputProps={{
-                                startAdornment: <InputAdornment position="start">
-                                    <SearchIcon fontSize="small" />
-                                </InputAdornment>
-                            }}
-                            onChange={this.handleSearch}
 
-                        >
-                        </TextField>
-                    </Toolbar>
+            <TableWithServerSidePagingAndSorting
+                pagingAndSorting={pagingAndSorting}
+                setPagingAndSorting={setPagingAndSorting}
+                headCells={headCells}
+                isLoading={false}
+            >
 
-                </Paper>
+                <TableBody>
 
-                <TableContainer component={Paper}  >
-                    <Table aria-label="collapsible table" className={classes.table}>
-
-                        <TableHead>
-                            {clients.length != 0 ? (
-                                <TableRow>
-                                    <TableCell />
-                                    {
-
-                                        headCells.map(headCell => (
-                                            <TableCell align="right" key={headCell.id}>
-                                                {headCell.disableSorting ? headCell.title :
-                                                    <TableSortLabel
-                                                        active={orderBy == headCell.id}
-                                                        direction={orderBy == headCell.id ? order : 'asc'}
-                                                        onClick={() => (this.handleSortRequest(headCell.id))}
-
-                                                    >
-                                                        {headCell.title}
-                                                    </TableSortLabel>
-                                                }
-                                            </TableCell>
-
-                                        ))
-
-                                    }
-                                </TableRow>
-
-                            )
-                                :
-                                (
-
-                                    <TableRow>
-                                        < Typography variant="h6" gutterBottom component="div" style={{ textAlign: 'center' }} >
-                                            Все още няма добавени клиенти.
-                                        </Typography>
-                                        <Typography>
-                                            <Button size="large">
-                                                <AddCircleOutlineIcon />
-                                                Добави Клиент
-                                            </Button>
-                                        </Typography>
-
-                                    </TableRow>
-
-                                )}
-                        </TableHead>
-
-                        <TableBody>
-                            {this.clientsAfterPagingAndSorting().map((client) => (
-                                <ClientRow key={client.id} {...client} align="right" />
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    page={page}
-                    component="div"
-                    rowsPerPageOptions={pages}
-                    rowsPerPage={rowsPerPage}
-                    count={this.state.clients.length}
-                    onChangePage={this.handleChangePage}
-                    onChangeRowsPerPage={this.rowsPerPageHandle}
-                />
+                    {clients.map((client) => (
+                        <ClientRow
+                            key={client.id}
+                            {...client}
+                            blockClientHandler={blockClientHandler}
+                            activateClientHandler={activateClientHandler}
+                            align="right"
+                        />
+                    ))}
+                </TableBody>
 
 
 
-            </ >
-        )
+            </TableWithServerSidePagingAndSorting>
+            <TablePagination
+                page={paging.page}
+                component="div"
+                rowsPerPageOptions={pages}
+                rowsPerPage={paging.rowsPerPage}
+                count={clients.length}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={trowsPerPageHandle}
+            />
 
-    }
-
+            <ConfirmationPopup
+                setOpenPopup={setOpenConfirmationPopup}
+                actionAfterConfirmation={() => { setUpdateClientStatusTriger(true) }}
+                openPopup={openConfirmationPopup}
+                title='Блокиране на клиента'
+                //disableUserPutTriger={disableUserPutTriger}
+                question={`Сигурни ли сте че искате да ограничите продажбите за ${clients.find(x => x.id === selectedUserId)?.name}`}
+            />
+        </>
+    )
 }
-export default withStyles(useStyles)(ClientList);
-
-
-
-
 
 
 
--- Client/app/src/components/Main/Clients/ClientList/ClientRow.js
+++ Client/app/src/components/Main/Clients/ClientList/ClientRow.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-
-import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom'
+import { currencyFormater } from '../../../../services/globalServices'
+import { withStyles, } from '@material-ui/core/styles';
+import FiberNewIcon from '@material-ui/icons/FiberNew';
 import Box from '@material-ui/core/Box';
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
@@ -18,7 +19,11 @@ import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import InfoIcon from '@material-ui/icons/Info';
 import MailIcon from '@material-ui/icons/Mail';
 import ReceiptIcon from '@material-ui/icons/Receipt';
-import { LinkedCamera } from '@material-ui/icons';
+import { convertCompanyName } from '../../../../services/globalServices'
+import BlockIcon from '@material-ui/icons/Block';
+import CheckIcon from '@material-ui/icons/Check';
+import {clientStatusFormater,invoiceStatusConverter} from '../../../../services/globalServices'
+
 
 
 
@@ -39,23 +44,22 @@ class ClientRow extends React.Component {
             open: false
         }
 
-        
+
 
     }
-   
-    setOpen() {
-      
 
+    setOpen() {
         this.setState((state) => {
             return { open: !this.state.open }
         })
     }
 
-    //[{name:"Vladis",vatNumber:"BG213123123" ,invoiceCount:"10",countOfoverdueInvoices:"1"}]
+
     render() {
         const { open } = this.state
         const { classes } = this.props;
-        const { name, vatNumber, countOfInvoice, countOfOverdueInvoices, invoices } = this.props
+        const { name, vatNumber, countOfInvoices, countOfOverdueInvoices, invoices,
+             companyType, valueSumOfAllUnPaidInvoices, status,blockClientHandler,id,activateClientHandler } = this.props
         return (
             <React.Fragment>
                 <TableRow className={classes.root} onClick={this.clickHandler}>
@@ -65,29 +69,50 @@ class ClientRow extends React.Component {
                         </IconButton>
                     </TableCell>
                     <TableCell component="th" scope="row">
-                        {name}
+                        {convertCompanyName({ name, companyType })}
                     </TableCell>
 
-                    <TableCell align="right">{vatNumber}</TableCell>
-                    <TableCell align="right">{countOfInvoice}</TableCell>
-                    <TableCell align="right">{countOfOverdueInvoices}</TableCell>
+                    <TableCell align="center">{vatNumber}</TableCell>
+                    <TableCell align="center" style={{ color: status == 'Blocked' ? '#FF0000' : '#0AE209' }}>
+                    {clientStatusFormater(status)}</TableCell>
+                    <TableCell align="center">{countOfInvoices}</TableCell>
+                    <TableCell align="center">{countOfOverdueInvoices}</TableCell>
+                    <TableCell align="center">{currencyFormater(valueSumOfAllUnPaidInvoices)}</TableCell>
                     <TableCell align="right">
-                        <Link to={`/Clients/ClientInfo/${this.props.id}`}  >
-                        <Button size="small" >
-                            <InfoIcon fontSize="small" />
-                        </Button>
+                        <Link to={`/Clients/ClientInfo/${this.props.id}`}   style={{ textDecoration: 'none' }}>
+                            <IconButton size="small" >
+                                <InfoIcon fontSize="medium" htmlColor='black' />
+                            </IconButton>
                         </Link>
-                        <Button size="small" onClick={this.deleteHandler}>
-                            <MailIcon fontSize="small" />
-                        </Button>
-                        <Button size="small" onClick={this.deleteHandler}>
-                            <ReceiptIcon fontSize="small" />
-                        </Button>
+                        <IconButton size="medium" onClick={this.deleteHandler}>
+                            <MailIcon fontSize="medium" htmlColor='black' />
+                        </IconButton>
+                        <Link to={`/Invoices/NewInvoice?clientId=${this.props.id}`}  >
+                            <IconButton size="medium" >
+                                <FiberNewIcon fontSize="medium" htmlColor='black' />
+                            </IconButton>
+                        </Link>
+                        {
+                            status === 'Blocked' ?
+                                (
+                                    <IconButton size="medium" onClick={(e)=>{activateClientHandler(e,id)}}>
+                                        <CheckIcon fontSize="medium" htmlColor='green' />
+                                    </IconButton>
+                                )
+                                :
+                                (
+                                    <IconButton size="medium" onClick={(e)=>{blockClientHandler(e,id)}} >
+                                        <BlockIcon fontSize="medium" htmlColor='red' />
+                                    </IconButton>
+                                )
+
+                        }
+
                     </TableCell>
                 </TableRow>
                 <TableRow >
 
-                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={8}>
                         <Collapse in={open} timeout="auto" unmountOnExit>
                             <Box margin={1}>
                                 <Typography variant="h6" gutterBottom component="div" style={{ textAlign: 'center' }} >
@@ -99,10 +124,11 @@ class ClientRow extends React.Component {
                                 {invoices.length != 0 && <Table size="small" aria-label="purchases">
                                     <TableHead>
                                         <TableRow>
-                                            <TableCell>Номер на фактурата</TableCell>
+                                            <TableCell>Фактура 	&#8470; </TableCell>
                                             <TableCell>Дата на издаване</TableCell>
                                             <TableCell>Дата на падеж</TableCell>
-                                            <TableCell>Цена</TableCell>
+                                            <TableCell>Статус</TableCell>
+                                            <TableCell>Цена </TableCell>
 
                                         </TableRow>
                                     </TableHead>
@@ -110,15 +136,16 @@ class ClientRow extends React.Component {
                                         {invoices.map((invoice) => (
                                             <TableRow key={invoice.id}>
                                                 <TableCell component="th" scope="row">
-                                                    {invoice.number}
+                                                    {invoice.invoiceNumber}
                                                 </TableCell>
                                                 <TableCell>{invoice.dateOfIssue}</TableCell>
-                                                <TableCell align="right">{invoice.paymentDueDate}</TableCell>
-                                                <TableCell align="right">{invoice.price}</TableCell>
+                                                <TableCell align="left">{invoice.paymentDueDate}</TableCell>
+                                                <TableCell align="left">{invoiceStatusConverter(invoice.status)}</TableCell>
+                                                <TableCell align="center">{currencyFormater(invoice.priceWithVat)}</TableCell>
                                             </TableRow>
                                         ))}
                                     </TableBody>
-                                </Table> }
+                                </Table>}
 
                             </Box>
                         </Collapse>
