fb637551a7a7090c96de25806da1f358185292ab
Persiyan93 <persiyan.georgiev@gmail.com>
2021-08-19 23:46:22 +0300

    Add Cloud Service

---
+++ InvoiceGenerator/Services/InvoiceGenerator.Services.CloadStorageService/DropBoxService.cs
@@ -0,0 +1,48 @@
+﻿
+using Dropbox.Api;
+using Dropbox.Api.Files;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace InvoiceGenerator.Services.CloadStorageService
+{
+    public class DropBoxService : IDropBoxService
+    {
+        private readonly DropboxClient client;
+
+        public DropBoxService(string apiKey)
+        {
+            this.client = new DropboxClient(apiKey);
+        }
+
+        public async Task<string> UploadFileAsync(string folder, string fileName, string fileUri)
+        {
+            try
+            {
+                using (var mem = new FileStream(fileUri, FileMode.Open, FileAccess.Read))
+                {
+                    var full = this.client.Users.GetCurrentAccountAsync();
+                    FileMetadata updated = await this.client.Files.UploadAsync(
+                        "/"+folder + "/" + fileName,
+                        WriteMode.Overwrite.Instance,
+                        body: mem);
+
+
+                    var arg1 = new Dropbox.Api.Sharing.CreateSharedLinkWithSettingsArg("/"+folder + "/" + fileName);
+                    var share = await this.client.Sharing.CreateSharedLinkWithSettingsAsync(arg1);
+                    return share.Url;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.Write(e);
+                throw;
+            }
+       
+           
+        }
+
+    }
+}
---
+++ InvoiceGenerator/Services/InvoiceGenerator.Services.CloadStorageService/IDropBoxService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceGenerator.Services.CloadStorageService
+{
+    public interface IDropBoxService
+    {
+
+        Task<string> UploadFileAsync(string folder,string fileName,string fileUri);
+    }
+}
