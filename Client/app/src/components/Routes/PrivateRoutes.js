84d404989606ea6d7271bc65a75340ebbe5a42fd
Persiyan93 <persiyan.georgiev@gmail.com>
2021-08-23 20:07:32 +0300

    Add routes components

---
+++ Client/app/src/components/Routes/PrivateRoutes.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+             
+export default function PrivateRoutes({ component :Component, user,...rest}) {
+   
+  
+  
+    return (
+       
+        <Route
+            {...rest}
+            render={(props) =>
+               user.isAuthenticated
+                    ? <Component {...props} />
+                    : <Redirect to={{ pathname: '/Identity/Login', state: { from: props.location } }} />
+
+            }
+        />
+    )
+}
---
+++ Client/app/src/components/Routes/ProductRoutes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+             
+export default function PrivateRoutes({ component :Component, user,...rest}) {
+   
+
+    return (
+        <Route
+            {...rest}
+            render={(props) =>
+                (user.isAuthenticated&& user.permissions.productsAccess)
+                    && <Component {...props} />
+                    // : <Redirect to={{ pathname: '/Identity/Login', state: { from: props.location } }} />
+
+            }
+        />
+    )
+}
